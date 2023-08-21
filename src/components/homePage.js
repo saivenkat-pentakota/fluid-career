11f4745e6f0b78a6b64dc1cc6002eb1ac66de318
Saivenkat Pentakota <saivenkatpentakota@gmail.com>
2023-08-21 15:53:12 +0530

    created react app fluid-career and installed and initialized firebase.

---
+++ src/components/homePage.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const homePage = () => {
+    return(
+        <div className="homePage">
+            <h1>Home Page</h1>
+        </div>
+    );
+}
---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAvmwAiWdYxkI2w7_RzmMIyPt6dNaxUbGM",
+  authDomain: "fluidcareer-3de8c.firebaseapp.com",
+  projectId: "fluidcareer-3de8c",
+  storageBucket: "fluidcareer-3de8c.appspot.com",
+  messagingSenderId: "768781781409",
+  appId: "1:768781781409:web:90435f87e7bd8f205563e0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
